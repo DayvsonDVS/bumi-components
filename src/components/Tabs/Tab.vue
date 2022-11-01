e1b4bfa8d8fc8857caa8a851582394f2a59f1513
DayvsonDvs <dayvson_dvs@hotmail.com>
2022-11-01 17:41:14 -0300

    refact: Tabs and tab

--- src/components/Tabs/Tab.vue
+++ src/components/Tabs/Tab.vue
@@ -1,81 +1,23 @@
 <template>
-  <div class="tab">
-    <button class="button" ref="buttonTitle" @click="openSlot(buttonTitle!)">
-      {{ title }}
-    </button>
-
-    <div class="slot-content">
-      <div class="shadow-button"></div>
-      <slot />
-    </div>
+  <div :class="['tab', { active: active }]" ref="tabs">
+    <slot />
   </div>
 </template>
 
 <script setup lang="ts">
-import { withDefaults, ref } from 'vue'
-import '@/assets/scss/vars.scss'
-
-const buttonTitle = ref<HTMLElement>()
-
-interface props {
-  title: string
-}
-
-withDefaults(defineProps<props>(), {})
-
-function openSlot(element: HTMLElement) {
-  classRemove()
-  element.classList.add('is-active')
-  element.nextElementSibling?.classList.add('is-open')
-}
-
-function classRemove() {
-  const buttons = [...document.getElementsByClassName('button')]
-
-  buttons.map((element) => {
-    element.classList.replace('is-active', 'button')
-    element.nextElementSibling?.classList.replace('is-open', 'slot-content')
-  })
-}
+defineProps(['active'])
 </script>
 
 <style scoped lang="scss">
 .tab {
-  display: grid;
-  .button {
-    z-index: 2;
-    color: #babdc5;
-    padding: 10px 2px 15px 2px;
-    border: none;
-    background: var(--outlined);
-    border-top-right-radius: 16px;
-    border-top-left-radius: 16px;
-    cursor: pointer;
-    transition: all 0.3s ease-in-out;
-    &.is-active {
-      background: #232832;
-    }
-  }
-  .slot-content {
-    padding: 20px 22px 18px;
-    background: #232832;
-    border-radius: 16px;
-    color: #babdc5;
-    left: 0;
-    right: 0;
-    margin: 0px 8px;
-    display: none;
-    position: absolute;
-    transition: all 0.3s ease-in-out;
-    .shadow-button {
-      background: var(--outlined);
-      height: 40px;
-      position: relative;
-      top: -20px;
-    }
-    &.is-open {
-      display: block;
-    }
+  display: none;
+  padding: 0.75rem;
+  color: #babdc5;
+  background: #232832;
+  border-bottom-left-radius: 12px;
+  border-bottom-right-radius: 12px;
+  &.active {
+    display: block;
   }
 }
 </style>
--- src/components/Tabs/Tabs.vue
+++ src/components/Tabs/Tabs.vue
@@ -1,15 +1,76 @@
 <template>
-  <div class="tabs">
-    <slot />
+  <div class="tabs" ref="tabContainer">
+    <div class="tabs-headers">
+      <ul>
+        <li
+          v-for="(tab, index) in tabs"
+          :key="index"
+          :class="activeTabIndex === index ? 'active' : ''"
+          @click="changeTab(index)"
+          ref="tabHeaders"
+        >
+          {{ tab.title }}
+        </li>
+      </ul>
+    </div>
+
+    <div class="active-tabs">
+      <slot />
+    </div>
   </div>
 </template>
+
 <script setup lang="ts">
-import '@/assets/scss/vars.scss'
+import { ref, onMounted } from 'vue'
+
+const tabContainer = ref()
+const tabHeaders = ref()
+const tabs = ref()
+const activeTabIndex = ref<number>()
+
+onMounted(() => {
+  tabs.value = [...tabContainer.value.querySelectorAll('.tab')]
+  for (let x of tabs.value) {
+    if (x.classList.contains('active')) {
+      activeTabIndex.value = tabs.value.indexOf(x)
+    }
+  }
+})
+
+const changeTab = (index: number) => {
+  activeTabIndex.value = index
+
+  for (let x of [...tabs.value, ...tabHeaders.value]) {
+    x.classList.remove('active')
+  }
+  tabs.value[activeTabIndex.value].classList.add('active')
+  tabHeaders.value[activeTabIndex.value].classList.add('active')
+}
 </script>
 
 <style scoped lang="scss">
-.tabs {
-  display: grid;
-  grid-auto-flow: column;
+.tabs-headers {
+  ul {
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    background: var(--outlined);
+    color: #babdc5;
+    width: 100%;
+    li {
+      list-style: none;
+      padding: 1rem 1.25rem;
+      cursor: pointer;
+      opacity: 0.5;
+      &.active {
+        opacity: 1;
+        background: #232832;
+        border-top-left-radius: 15px;
+        border-top-right-radius: 15px;
+      }
+    }
+  }
 }
 </style>
